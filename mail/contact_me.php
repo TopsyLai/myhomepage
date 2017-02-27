636aaf6c0f8670bda74b291c12dc7e62b0203f07
kobe <471816299@qq.com>
2017-02-27 16:47:14 +0800

    all

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,7 +9,6 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-
 $name = $_POST['name'];
 $email_address = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
 if ($email === FALSE) {
@@ -18,14 +17,12 @@ if ($email === FALSE) {
 }
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-
-
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
+$to = '471816299@.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "联系方式来自网页版简历的:  $name";
+$email_body = "您收到一份邮件来自: $name\n\邮件地址: $email_address\n\n手机号码: $phone\n\n信息:\n$message";
+$headers = "收件人：471816299@qq.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "发件人: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
